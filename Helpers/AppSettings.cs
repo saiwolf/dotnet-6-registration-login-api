b3db79e47d69126f3de4478a054b7f2639d15a36
Robert E. Cato <robertcato015@gmail.com>
2022-01-15 13:32:09 -0600

    Added Serilog and Swashbuckle (for Swagger). Auth works.

--- Helpers/AppSettings.cs
+++ Helpers/AppSettings.cs
@@ -1,6 +1,20 @@
+using MailKit.Security;
+
 namespace WebApi.Helpers;
 
 public class AppSettings
 {
     public string Secret { get; set; }
+    public EmailSettings EmailSettings { get; set; }
+    
+}
+
+public class EmailSettings
+{
+    public string Host { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public bool SmtpAuthRequired { get; set; }
+    public int Port { get; set; }
+    public SecureSocketOptions SecureSocketOptions { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,59 +1,121 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Serilog;
+using System.Reflection;
 using WebApi.Authorization;
 using WebApi.Helpers;
 using WebApi.Services;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-// add services to DI container
+Log.Information($"Starting API at {DateTime.Now}");
+
+try
 {
-    var services = builder.Services;
-    var env = builder.Environment;
- 
-    // use sql server db in production and sqlite db in development
-    if (env.IsProduction())
-        services.AddDbContext<DataContext>();
-    else
-        services.AddDbContext<DataContext, SqliteDataContext>();
- 
-    services.AddCors();
-    services.AddControllers();
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog((ctx, lc) => lc
+        .WriteTo.Console()
+        .ReadFrom.Configuration(ctx.Configuration));
 
-    // configure automapper with all automapper profiles from this assembly
-    services.AddAutoMapper(typeof(Program));
+    // add services to DI container
+    {
+        IServiceCollection services = builder.Services;
+        IWebHostEnvironment env = builder.Environment;
 
-    // configure strongly typed settings object
-    services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+        // use sql server db in production and sqlite db in development
+        if (env.IsProduction())
+            services.AddDbContext<DataContext>();
+        else
+            services.AddDbContext<DataContext, SqliteDataContext>();
 
-    // configure DI for application services
-    services.AddScoped<IJwtUtils, JwtUtils>();
-    services.AddScoped<IUserService, UserService>();
+        services.AddCors();
+        services.AddControllers();
+
+        // Only set up Swagger in Development Mode
+        if (env.IsDevelopment())
+        {
+            // Swagger Setup
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = ".NET 6 Registration and Login with JWT API",
+                    Description = "API for registration and log using JWT auth.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Robert Cato",
+                        Email = "saiwolf@swmnu.net",
+                        Url = new Uri("https://github.com/saiwolf"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "MIT",
+                        Url = new Uri("https://opensource.org/licenses/MIT")
+                    },
+                });
+
+                string xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+            });
+        }
+
+        // configure automapper with all automapper profiles from this assembly
+        services.AddAutoMapper(typeof(Program));
+
+        // configure strongly typed settings object
+        services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+
+        // configure DI for application services
+        services.AddScoped<IJwtUtils, JwtUtils>();
+        services.AddScoped<IUserService, UserService>();
+    }
+
+    WebApplication app = builder.Build();
+
+    if (builder.Environment.IsDevelopment())
+    {
+        // migrate any database changes on startup (includes initial db creation)
+        using IServiceScope scope = app.Services.CreateScope();
+        DataContext dataContext = scope.ServiceProvider.GetRequiredService<DataContext>();
+        dataContext.Database.Migrate();
+    }
+    
+
+    // configure HTTP request pipeline
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        // global cors policy
+        app.UseCors(x => x
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
+        // global error handler
+        app.UseMiddleware<ErrorHandlerMiddleware>();
+
+        // custom jwt auth middleware
+        app.UseMiddleware<JwtMiddleware>();
+
+        app.MapControllers();
+    }
+
+    app.Run("http://localhost:4000");
 }
-
-var app = builder.Build();
-
-// migrate any database changes on startup (includes initial db creation)
-using (var scope = app.Services.CreateScope())
+catch (Exception ex)
 {
-    var dataContext = scope.ServiceProvider.GetRequiredService<DataContext>();    
-    dataContext.Database.Migrate();
+    Log.Fatal(ex, ex.Message);
 }
-
-// configure HTTP request pipeline
+finally
 {
-    // global cors policy
-    app.UseCors(x => x
-        .AllowAnyOrigin()
-        .AllowAnyMethod()
-        .AllowAnyHeader());
-
-    // global error handler
-    app.UseMiddleware<ErrorHandlerMiddleware>();
-
-    // custom jwt auth middleware
-    app.UseMiddleware<JwtMiddleware>();
-
-    app.MapControllers();
+    Log.Information($"API Shutdown at {DateTime.Now}");
+    Log.CloseAndFlush();
 }
-
-app.Run("http://localhost:4000");
